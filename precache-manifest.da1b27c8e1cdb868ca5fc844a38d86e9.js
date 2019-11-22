5c73266cb40b0cbe7a4449f8cc1a91791cf941af
Artur <grandfaraon93@gmail.com>
2019-11-22 12:52:31 +0200

    Updates

---
+++ precache-manifest.da1b27c8e1cdb868ca5fc844a38d86e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4476ba09a85905c15677bdb9bddaea15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbab95d84fe0b4d5e0ba",
+    "url": "/scripts/bundle.c8fc358.js"
+  },
+  {
+    "revision": "f7b53f864fa43c99378c93e31f94dd3b",
+    "url": "/scripts/bundle.c8fc358.js.LICENSE"
+  },
+  {
+    "revision": "97632830a7dad88b6b5c",
+    "url": "/scripts/modernizr.c8fc358.js"
+  },
+  {
+    "revision": "98fe731926ca7c3ac473a550f124342a",
+    "url": "/scripts/modernizr.c8fc358.js.LICENSE"
+  },
+  {
+    "revision": "3e0216a8a635c2aed765701e35802673",
+    "url": "/static/icon.c8fc358.svg"
+  },
+  {
+    "revision": "8d362760b25279384e2466f9bd832ce8",
+    "url": "/static/react-redux-saga.c8fc358.svg"
+  }
+]);
