c822a8b9049e051fe1d9ecddf373cdf743ca0a5b
Artur <grandfaraon93@gmail.com>
2019-11-23 14:16:22 +0200

    Updates

--- precache-manifest.6d41c8f2b75bfd19820fafd17cbbaa42.js
+++ precache-manifest.6d41c8f2b75bfd19820fafd17cbbaa42.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e95e272dd10716e516fdd022ffe182c7",
-    "url": "/GitAppindex.html"
+    "url": "/GitApp//index.html"
   },
   {
     "revision": "0242c6cacd47c3610d47",
-    "url": "/GitAppscripts/bundle.59a305c.js"
+    "url": "/GitApp//scripts/bundle.59a305c.js"
   },
   {
     "revision": "f7b53f864fa43c99378c93e31f94dd3b",
-    "url": "/GitAppscripts/bundle.59a305c.js.LICENSE"
+    "url": "/GitApp/scripts/bundle.59a305c.js.LICENSE"
   },
   {
     "revision": "49ccaefc0b0626aa8a23",
-    "url": "/GitAppscripts/modernizr.59a305c.js"
+    "url": "/GitApp/scripts/modernizr.59a305c.js"
   },
   {
     "revision": "98fe731926ca7c3ac473a550f124342a",
-    "url": "/GitAppscripts/modernizr.59a305c.js.LICENSE"
+    "url": "/GitApp/scripts/modernizr.59a305c.js.LICENSE"
   },
   {
     "revision": "3e0216a8a635c2aed765701e35802673",
-    "url": "/GitAppstatic/icon.59a305c.svg"
+    "url": "/GitApp/static/icon.59a305c.svg"
   },
   {
     "revision": "8d362760b25279384e2466f9bd832ce8",
-    "url": "/GitAppstatic/react-redux-saga.59a305c.svg"
+    "url": "/GitApp/static/react-redux-saga.59a305c.svg"
   }
 ]);
