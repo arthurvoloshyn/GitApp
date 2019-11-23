fb1313e516d897817b40994a5560bff98e22d92c
Artur <grandfaraon93@gmail.com>
2019-11-23 13:37:13 +0200

    Updates

---
+++ precache-manifest.1a1588684872ec21fd3fb4d8a8e4d4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e2d5a5211f8f8a478151ce5994def7",
+    "url": "/GitAppindex.html"
+  },
+  {
+    "revision": "77220ea96a2cd8f7bdda",
+    "url": "/GitAppscripts/bundle.59a305c.js"
+  },
+  {
+    "revision": "f7b53f864fa43c99378c93e31f94dd3b",
+    "url": "/GitAppscripts/bundle.59a305c.js.LICENSE"
+  },
+  {
+    "revision": "49ccaefc0b0626aa8a23",
+    "url": "/GitAppscripts/modernizr.59a305c.js"
+  },
+  {
+    "revision": "98fe731926ca7c3ac473a550f124342a",
+    "url": "/GitAppscripts/modernizr.59a305c.js.LICENSE"
+  },
+  {
+    "revision": "3e0216a8a635c2aed765701e35802673",
+    "url": "/GitAppstatic/icon.59a305c.svg"
+  },
+  {
+    "revision": "8d362760b25279384e2466f9bd832ce8",
+    "url": "/GitAppstatic/react-redux-saga.59a305c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c77094212073c1ec038f690c76751462.js"
+  "/GitAppprecache-manifest.1a1588684872ec21fd3fb4d8a8e4d4c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GitAp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
