79a59d34bc73adccaa5c6c3d3875c1546569f219
Artur <grandfaraon93@gmail.com>
2019-11-23 13:57:47 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 importScripts(
-  '/GitApp/precache-manifest.1a1588684872ec21fd3fb4d8a8e4d4c1.js'
+  '/GitAppprecache-manifest.1a1588684872ec21fd3fb4d8a8e4d4c1.js'
 );
 
 self.addEventListener('message', (event) => {
