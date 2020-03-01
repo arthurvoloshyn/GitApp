12d557f7001417ba193bbdcb23a7d900cbe43ca0
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-01 16:09:53 +0200

    Edited build directory

--- config/paths.js
+++ config/paths.js
@@ -21,7 +21,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   appPath: resolvePath('.'),
   appAssets: resolvePath('assets'),
-  appBuild: resolvePath('build'),
+  appBuild: resolvePath('dist'),
   appHtml: resolvePath('assets/index.html'),
   appIndexJs: resolveModule(resolvePath, 'src/index'),
   appModernizr: resolvePath('src/vendor/modernizr-custom.js'),
